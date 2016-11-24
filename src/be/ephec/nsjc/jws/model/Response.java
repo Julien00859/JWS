cd8a2924157446f05562d5226db6279e05071486
Julien00859 <Julien.castiaux@gmail.com>
2016-11-24 23:25:41 +0100

    Fix #2

--- src/be/ephec/nsjc/jws/model/Response.java
+++ src/be/ephec/nsjc/jws/model/Response.java
@@ -5,78 +5,78 @@ public class Response {
     private String body;
     private ReponseCode HTTPCode;
 
+    /** Full constructor of Reponse
+     * @param code : the HTTP code
+     * @param headers : a list of HTTP headers
+     * @param body : the HTTP body
+     */
     public Response(ResponseCode HTTPCode, HashMap<String, String> headers, String body) {
-        /** Full constructor of Reponse
-         * param code : the HTTP code
-         * param headers : a list of HTTP headers
-         * param body : the HTTP body
-         */
         this.HTTPCode = HTTPCode;
         this.headers = headers;
         this.body = body;
     }
 
+    /** Constructor of Response without any header
+     * @param code : the HTTP code
+     * @param body : the HTTP body
+     */
     public Response(ResponseCode HTTPCode, char[] body) {
-        /** Constructor of Response without any header
-         * param code : the HTTP code
-         * param body : the HTTP body
-         */
         this.HTTPCode = HTTPCode;
         this.body = body;
     }
 
+    /** Constructor of Reponse without any headers and withut body
+     * @param code: the HTTP code
+     */
     public Response(ResponseCode code) {
-        /** Constructor of Reponse without any headers and withut body
-         * param code: the HTTP code
-         */
         this.code = code;
     }
 
+    /** Append a header to the current list of headers
+     * @param header : the header to append
+     */
     public void addHeader(Header header) {
-        /** Append a header to the current list of headers
-         * param header : the header to append
-         */
         this.headers.set(header.label, header.value);
     }
 
+    /** Remove a header from the current list of headers
+     * @param label : the header label to remove
+     * @return : a Header with the label and its value if it exists or null if it doesn't
+     */
     public Header delHeader(String label) {
-        /** Remove a header from the current list of headers
-         * param label : the header label to remove
-         * return : a Header with the label and its value if it exists or null
-         */
         if (this.headers.containsKey(label)) {
             return new Header(label, this.headers.remove(label));
         }
         return null;
     }
 
+    /** Check if the header is in the list of headers
+     * @param header : the header that will be checked
+     * @return : whether the object is in the hashmap or not
+     */
     public boolean hasHeader(Header header) {
-        /** Check if the header is in the list of headers
-         * param header : the header that will be checked
-         * return : whether the object is in the hashmap or not
-         */
-        return this.hasHeader(header.label);
+        return this.hasHeader(header.label) && this.headers.get(header.label) == header.value;
     }
 
+    /** Check if the label is in the list of headers
+     * @param headerLabel : the label that will be checked
+     * @return : whether the label is in the hashmap or not
+     */
     public boolean hasHeader(String headerLabel) {
-        /** Check if the label is in the list of headers
-         * param headerLabel : the label that will be checked
-         * return : whether the label is in the hashmap or not
-         */
         return this.headers.containsKey(headerLabel);
     }
 
+    /** Convert the response to a sendable format
+     * @return : a byte array of the current response
+     */
     public byte[] toByteArray() {
-        /** Convert the response to a sendable format
-         * return : a byte array of the current response
-         */
         return this.toString().getBytes(StandardCharsets.UTF_8);
     }
 
+    /** Convert the response to a printable format
+     * @return : a string of the current response
+     */
     public String toString() {
-        /** Convert the response to a printable format
-         * return : a string of the current response
-         */
         String res = "";
         res += "HTTP1.1 " + this.HTTPCode.code + " " + this.HTTPCode.desc + "\r\n";
         for (String label in this.headers) {
