f39aff17a0623e9e4356b8234b33c3f083f8c128
Julien00859 <Julien.castiaux@gmail.com>
2016-11-29 17:04:11 +0100

    definition of fileUtils

--- src/be/ephec/nsjc/jws/internal/FileUtils.java
+++ src/be/ephec/nsjc/jws/internal/FileUtils.java
@@ -1,5 +1,26 @@
 package be.ephec.nsjc.jws.internal;
 
+import java.io.File;
+import java.nio.file.Files;
+
 public class FileUtils {
+
+    public static String baseDir = "www" + File.separator;
+
+    /** For a filename given give the content
+     * @param uri the relative path from the base dir
+     * @return le content of the file
+     */
+    public static byte[] getFile(String uri) {
+        return null;
+    }
+
+    /** Save on the disk a file at the uri given with the content
+     * @param uri the relative path from the base dir
+     * @param content the content of the file
+     */
+    public static void postFile(String uri, byte[] content) {
+
+    }
 
 }
