3f85775448796ad600bbb85bef920b25007bfd86
izanagi1995 <nsurleraux@gmail.com>
2016-11-29 18:40:16 +0100

    intermediate commit

--- src/be/ephec/nsjc/jws/controller/JWSController.java
+++ src/be/ephec/nsjc/jws/controller/JWSController.java
@@ -1,11 +1,24 @@
 package be.ephec.nsjc.jws.controller;
 
-import be.ephec.nsjc.jws.internal.ServerThread;
+import be.ephec.nsjc.jws.model.HTTPTrace;
 import be.ephec.nsjc.jws.view.AbstractView;
 
 public class JWSController {
-	AbstractView view;
-	ServerThread sThread;
+	private AbstractView view;
+	private HTTPTrace trace;
+	
+	public JWSController(AbstractView view, HTTPTrace trace) {
+		this.view = view;
+		this.trace = trace;
+	}
+	
+	public void logToView(String log){
+		view.log(log);
+	}
+	
+	public void handleInput(String reqLine){
+		
+	}
 	
 	
 }
