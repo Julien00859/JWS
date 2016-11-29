773cc5ac3902c376b4d26dc63140b965d455955d
Julien00859 <Julien.castiaux@gmail.com>
2016-11-29 17:31:39 +0100

    add deleteFile

--- src/be/ephec/nsjc/jws/internal/FileUtils.java
+++ src/be/ephec/nsjc/jws/internal/FileUtils.java
@@ -1,26 +1,46 @@
 package be.ephec.nsjc.jws.internal;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 
 public class FileUtils {
 
-    public static String baseDir = "www" + File.separator;
+    private static String baseDir = "www";
+    public static void setBaseDir(String baseDir) {
+        FileUtils.baseDir = baseDir;
+    }
+    public static String getBaseDir() {
+        return FileUtils.baseDir;
+    }
 
     /** For a filename given give the content
      * @param uri the relative path from the base dir
      * @return le content of the file
+     * @throws IOException if the reading failed
      */
-    public static byte[] getFile(String uri) {
-        return null;
+    public static byte[] getFileContent(String uri) throws IOException {
+        File f = getFile(uri);
+        return Files.readAllBytes(f.toPath());
     }
 
     /** Save on the disk a file at the uri given with the content
      * @param uri the relative path from the base dir
      * @param content the content of the file
+     * @throws IOException if the writing failed
      */
-    public static void postFile(String uri, byte[] content) {
+    public static void postFile(String uri, byte[] content) throws IOException {
+        File f = getFile(uri);
+        Files.write(f.toPath(), content);
+    }
 
+    /** Remove a file from the filesystem
+     * @param uri the relative path from the base dir
+     * @throws IOException if the delete failed
+     */
+    public static void deleteFile(String uri) throws IOException {
+        File f = getFile(uri);
+        Files.delete(f.toPath());
     }
 
     /** Check if the given uri is an existing file
@@ -28,7 +48,15 @@ public class FileUtils {
      * @return wheter the file exists or not
      */
     public static boolean fileExists(String uri) {
-        return true;
+        File f = getFile(uri);
+        return f.exists() && !f.isDirectory();
     }
 
+    /** Convert an URI to an OS path
+     * @param uri the relative path fromthe base dir
+     * @return the file at the given location
+     */
+    private static File getFile(String uri) {
+        return new File(baseDir + uri.replaceAll("/", File.separator));
+    }
 }
